a705a0e41f0ba95b1e53b5580dbd6ec360516fb6
katelarionova2058 <2dvedma@gmail.com>
2023-06-09 23:28:06 +0300

    Added function calculation_slae_roots

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def reducing_matrix_to_triangle(matrix: list, free_vector: list) -> list:
     free_vector_res: list = []
     return [matrix_res, free_vector_res]
 
-
 def calculation_slae_roots(matrix_res: list, free_vector_res: list) -> list:
     """
     Обратный ход. Решение СЛАУ.
@@ -17,6 +16,10 @@ def calculation_slae_roots(matrix_res: list, free_vector_res: list) -> list:
     :param free_vector_res: преобразованный вектор свободных членов.
     :return: список корней СЛАУ
     """
-    roots_slae = []
+    n = len(matrix_res)
+    roots_slae = [0]*n
+    for i in range(n-1, -1, -1):
+        roots_slae[i] = free_vector_res[i] / matrix_res[i][i]
+        for j in range(i-1, -1, -1):
+            free_vector_res[j] = free_vector_res[j] - matrix_res[j][i] * roots_slae[i]
     return roots_slae
-
