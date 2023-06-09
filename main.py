fec67c9ff6ba1781fb929a9bdcb3d977ab97866d
sovosof <baurina.sofi@gmail.com>
2023-06-09 21:57:23 +0300

    matrix_to_triangle without free_vector. with awful names. help meeee

--- main.py
+++ main.py
@@ -5,9 +5,56 @@ def reducing_matrix_to_triangle(matrix: list, free_vector: list) -> list:
     :param free_vector: свободный вектор (b)
     :return: треугольная матрица, преобразованный вектор свободных членов
     """
-    matrix_res: list = []
+
+    #zero_columns_count: int
+    matrix_final = []
+    for line in matrix:
+        matrix_final.append(line)
+
+    def column_to_zero(matrix):
+
+        matrix_res: list = []
+        zero_columns_count = 0
+
+        for raw in matrix[1::]:
+            new_raw: list = []
+            matrix_res.append(new_raw)
+            i = 0
+            for element in raw:
+                # if el=0 pass
+                a = matrix[0][i]
+                b = raw[0]
+                c = matrix[0][0]
+                new_element = element - a * (b / c)
+                new_raw.append(new_element)
+                i += 1
+        else:
+            zero_columns_count += 1
+        h = 1
+        for line_res in matrix_res[::-1]:
+            g = 1
+            for el in line_res[::-1]:
+                matrix_final[-h][-g] = el
+                g += 1
+            h += 1
+
+        if zero_columns_count < (len(matrix) - 1):
+            matrix_ = []
+            for part in matrix_res:
+                matrix_.append(list(part[1:]))
+            column_to_zero(matrix_)
+
+
+    column_to_zero(matrix)
     free_vector_res: list = []
-    return [matrix_res, free_vector_res]
+    return print([matrix_final])
+    #, free_vector_res
+reducing_matrix_to_triangle([[1, 2, 1], [3, -1, -1], [-2, 2, 3]], [0, 0, 11])
+reducing_matrix_to_triangle([[3, 2, 1, 1], [1, -1, 4, -1], [-2, -2, -3, 1], [1, 5, -1, 2]], [2, -1, 9, 4])
+
+
+
+
 
 
 def calculation_slae_roots(matrix_res: list, free_vector_res: list) -> list:
