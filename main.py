f1b6b9092bf8f2951b9228f557dff02d8a222b25
Mikhail <wacko.dead@gmail.com>
2023-06-10 00:26:17 +0300

    я попытался

--- main.py
+++ main.py
@@ -71,16 +71,15 @@ reducing_matrix_to_triangle([[3, 2, 1, 1], [1, -1, 4, -1], [-2, -2, -3, 1], [1, 
 
 def calculation_slae_roots(matrix_res: list, free_vector_res: list) -> list:
     """
-    Обратный ход. Решение СЛАУ.
-    :param matrix_res: треугольная матрица
-    :param free_vector_res: преобразованный вектор свободных членов.
-    :return: список корней СЛАУ
-    """
+        Обратный ход. Решение СЛАУ.
+        :param matrix_res: треугольная матрица
+        :param free_vector_res: преобразованный вектор свободных членов.
+        :return: список корней СЛАУ
+        """
     n = len(matrix_res)
-    roots_slae = [0]*n
-    for i in range(n-1, -1, -1):
+    roots_slae = [0] * n
+    for i in range(n - 1, -1, -1):
         roots_slae[i] = free_vector_res[i] / matrix_res[i][i]
-        for j in range(i-1, -1, -1):
+        for j in range(i - 1, -1, -1):
             free_vector_res[j] = free_vector_res[j] - matrix_res[j][i] * roots_slae[i]
     return roots_slae
-
